a760ba510fe29a5f6d5ed4503d6a804f14952b91
martin laville <spi3128@mordor.univ-lemans.fr>
2017-02-06 17:28:56 +0100

    bdd

--- Pokoko/pokedex/models.py
+++ Pokoko/pokedex/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Pokemon(models.Model):
+	numeroPokemon = models.IntegerField
+	nomPokemon = models.CharField(max_length=64)
+	generationPokemon = models.IntegerField
+	type1 = models.CharField(max_length=64)
+	type2 = models.CharField(max_length=64)
+	coutPokemon = models.IntegerField
