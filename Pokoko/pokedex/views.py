fef9ca7b8a28cec8c9860c9543fce6f0ca8d58c3
marvin jean <spi3124@ic2s124-09.univ-lemans.fr>
2017-02-07 10:50:15 +0100

    Ajout de dossier template + test de truc

--- Pokoko/pokedex/views.py
+++ Pokoko/pokedex/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
+#Page d'acceuil, a la racine du site
 def index(request):
 	
-    return HttpResponse("Hello")
+    return render(request,'/acceuil.html')
+
+#Page de sommaire, a /sommaire
+def sommaire(request):
+	return HttpResponse("Oep,le sommaire")
+
+#page d'un sommaire precis, a /sommaire/X
+def sommaire_note(request,id_sommaire):
+	if int(id_sommaire) > 100:
+		raise Http404
+
+	return HttpResponse("Tu demandes le sommaire {0} ? J'ai rien fait dmg.".format(id_sommaire))
+
+#Test pour afficher la date
+def date_actuelle(request):
+	return render(request, 'test/date.html',{'date':datetime.now()})
+
+#Test pour additionner
+def addition(request,nb1,nb2):
+	total = int(nb1) + int(nb2)
+
+	return render(request,'test/addition.html',locals())
