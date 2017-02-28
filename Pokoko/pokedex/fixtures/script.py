225a87fa8a51b4bedda0548fc28761f19b624380
Apeul <martinsla@live.fr>
2017-02-28 15:28:52 +0100

    Scrit pour convertir les fixtures

--- Pokoko/pokedex/fixtures/script.py
+++ Pokoko/pokedex/fixtures/script.py
@@ -1,6 +1,5 @@
 import json
 
-data = []
 pokedex_data=json.load(open("./pokemon.json"))
 
 
@@ -9,9 +8,19 @@ new_data = []
 #print (pokedex_data["values"])
 
 for value in pokedex_data["values"]:
-	new_data.append({"model": "myapp.person"})
+	new_data.append({
+		"model": "pokedex.pokemon",
+		"fields": {
+			"numero_pokemon": value[0],
+			"nom_pokemon": value[1],
+			"generation_pokemon": value[2],
+			"type_pokemon_1": value[3],
+			"type_pokemon_2": value[4],
+			"cout_pokemon": value[5]
+		}
+	})
 
-json_string=json.dumps(new_data,ensure_ascii=False, sort_keys=True,
+json_string=json.dumps(new_data,ensure_ascii=False,
 					   indent=4, separators=(',', ': '))	
 
 print (json_string)
